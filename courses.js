112495da68ddc487fdb594d983b4c57ceb6c9fc4
SalmanKhanK <m.salmank9991@gmail.com>
2023-03-26 03:20:48 -0700

    get and craete courses

---
+++ courses.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/SocialApp').then(()=>{
+    console.log("database Connected...")
+}).catch((err)=>{
+   console.log("error",err)
+});
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    price: Number,
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+ async function createCourses(){
+    const course = new Course({
+        name: 'Angular',
+        author: 'Alban ',
+        tags:['TypeScript', 'FrontEnd'],
+        price: 10,
+        isPublished: false
+    });
+    const result = await course.save();
+    console.log(result);
+ }
+ //createCourses();
+
+ async function getCourses(){
+    // const courses = await Course.find(); //simply return all query document
+    
+    const courses = await Course
+       // .find({author: 'Salman'})
+       // .find({price: {$gt: 10}})   //gte, gt, lte, lt, eq, ne, in, nin(not in)
+        .find()
+        .and([{author: 'Salman'},{isPublished: false}])  // logical operator (or, and)
+        .limit(10)
+        .sort({name: 1})   // 1 for asc and -1 for desc
+        .select({name: -1, tags: 1, author: 1, isPublished: 1});  // select those properties that u want to display
+
+
+    console.log(courses);
+ }
+
+ getCourses();
